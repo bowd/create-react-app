008c043df1646038ada2eda08cb2ac294e8ec0e9
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2018-10-17 13:22:04 +0100

    added err handling for rm cmd (Windows issue)

--- packages/react-scripts/scripts/utils/rustCompiler.js
+++ packages/react-scripts/scripts/utils/rustCompiler.js
@@ -1,8 +1,13 @@
 const execSync = require('child_process').execSync;
 
 module.exports = () => {
-  execSync(
-    'rustc +nightly --target wasm32-unknown-unknown -O --crate-type=cdylib src/lib.rs -o public/app.big.wasm && wasm-gc public/app.big.wasm public/app.wasm'
-    // 'rustc +nightly --target wasm32-unknown-unknown -O --crate-type=cdylib src/lib.rs -o public/app.big.wasm && wasm-gc public/app.big.wasm public/app.wasm && rm public/app.big.wasm'
-  );
+  try {
+    execSync(
+      'rustc +nightly --target wasm32-unknown-unknown -O --crate-type=cdylib src/lib.rs -o public/app.big.wasm && wasm-gc public/app.big.wasm public/app.wasm && rm public/app.big.wasm'
+    );
+  } catch (error) {
+    execSync(
+      'rustc +nightly --target wasm32-unknown-unknown -O --crate-type=cdylib src/lib.rs -o public/app.big.wasm && wasm-gc public/app.big.wasm public/app.wasm'
+    );
+  }
 };
