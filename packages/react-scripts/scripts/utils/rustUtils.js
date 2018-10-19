4975e02fb3099c01f072a0d63b7cda828b18b4ce
Thomas Horrobin <thomas.horrobin@stepstone.com>
2018-10-19 11:25:41 +0100

    renamed lib.rs to App.rs

--- packages/react-scripts/scripts/utils/rustUtils.js
+++ packages/react-scripts/scripts/utils/rustUtils.js
@@ -5,7 +5,7 @@ module.exports = {
     // first build app.big.wasm and then optimize to app.wasm with wasm-gc
     try {
       execSync(
-        'rustc +nightly --target wasm32-unknown-unknown -O --crate-type=cdylib src/lib.rs -o public/app.big.wasm && wasm-gc public/app.big.wasm public/app.wasm && rm public/app.big.wasm'
+        'rustc +nightly --target wasm32-unknown-unknown -O --crate-type=cdylib src/App.rs -o public/app.big.wasm && wasm-gc public/app.big.wasm public/app.wasm && rm public/app.big.wasm'
       );
     } catch (error) {
       // on Windoze sometimes 'rm' is not reliable. if it fails try with this
--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import loadAdd from './lib.rs';
+import loadAdd from './App.rs';
 
 class App extends Component {
   constructor() {
